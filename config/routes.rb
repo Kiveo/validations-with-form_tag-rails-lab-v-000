76cc7875ec6d7a5935da5844ee2d517f8732ddff
Maverick Tesini <mavericktesini@gmail.com>
2018-07-30 21:14:06 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+# <<<<<<< HEAD
   resources :authors
   resources :posts
-=======
+# =======
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :authors
